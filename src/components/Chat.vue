14eccc9d4222e9ee58b248624859a3167c322f5b
PING NGE <ping.nge@hotmail.com>
2020-09-10 13:58:59 +1000

    UPDATE: added welcome and chat components

---
+++ src/components/Chat.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="chat container">
+    <h2>Chat</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Chat',
+    props: ['name'],
+    data() {
+      return {
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Chat from '../components/Chat.vue'
 
 Vue.use(VueRouter)
 
@@ -11,13 +12,11 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/chat',
+    name: 'Chat',
+    component: Chat,
+    props: true
+  },
 ]
 
 const router = new VueRouter({
